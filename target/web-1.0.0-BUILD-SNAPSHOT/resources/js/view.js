ac7fc55c41838e99884358d015505c850c0adaff
Minwoo <kurekure900219@gmail.com>
2018-04-17 10:38:59 +0900

    first test

---
+++ target/web-1.0.0-BUILD-SNAPSHOT/resources/js/view.js
@@ -0,0 +1,77 @@
+// Common Tag for Dynamic
+var createATag=x=>{
+	return '<a id="'+x.id+'" class="'+x.clazz+'" href="#">'+x.val+'</a>';
+};
+var createITag=x=>{
+	return '<i id="'+x.id+'" class="'+x.clazz+'"></i>';
+};
+var createGlyphicon=x=>{
+	return '<span class="glyphicon ' +x.clazz+'" aria-hidden="true">&nbsp;'+x.val+'</span>'
+};
+var createSpan=x=>{
+	return '<span class="'+x.clazz+'">'+x.val+'</span>';
+};
+var createHTag=x=>{
+	return '<h'+x.num+'>'+x.val+'</h'+x.num+'>';
+};
+var createTab=x=>{
+	return '<table id="'+x.id+'" class="table table-'+x.clazz+'"></table>';
+};
+var createThead=x=>{
+	return '<thead>'+x+'</thead>';
+};
+var createTbody=x=>{
+	return '<tbody>'+x+'</tbody>';
+};
+var createTh=x=>{
+	var t = '<tr>';
+	$.each(x.list, (i, j)=>{
+		t += '<th>'+j+'</th>';
+	});
+	t += '</tr>';
+	return t;
+};
+var createTr=x=>{
+	var t = '';
+	$.each(x.list, (i, j)=> {
+		t += '<tr>'+createTd({seq : j.bbsSeq, list : j})+'</tr>';
+	});
+	return t;
+};
+var createTd=x=>{
+	var t = '';
+	var seq = x.seq;
+	$.each(x.list, (i, j)=>{
+		if(i !== 'content'){
+			if(i === 'title'){
+				t += '<td><a id="a-'+seq+'">'+j+'</a></td>';
+			} else {
+				t += '<td>'+j+'</td>';
+			}
+		}
+	});
+	return t;
+};
+var createUL=x=>{
+	return '<ul id="'+x.id+'" class="'+x.clazz+'"></ul>';
+	}
+var createLI=x=>{
+	return '<li id="'+x.id+'" class="'+x.clazz+'"></li>';
+}
+var createInput=x=>{
+	return '<input type="'+x.type+'" id="'+x.id+'"class="'+x.clazz
+      +' "value="'+x.placeholder+'">';
+}
+var createBtn=x=>{
+	return '<button id="'+ x.id +'" class="'+ x.clazz +'">'+ x.val +'</button>';
+};
+var createForm=x=>{
+	return '<form id="'+ x.id +'" class="'+ x.clazz +
+		'action="'+ x.action +'" method="'+ x.method +'"></form>';
+};
+var createDiv=x=>{
+	return '<div id="'+ x.id +'" class="'+ x.clazz +'"></div>';
+};
+var createImg=x=>{
+	return '<img id="'+x.id+'" class="'+x.clazz+'" src="'+x.src+'" alt="'+x.alt+'" />'
+};
