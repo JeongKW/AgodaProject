c0f35b41ea99373ba7fe002b0c399b4f9aa0eeea
Minwoo <kurekure900219@gmail.com>
2018-04-23 12:18:31 +0900

    2018-04-23 12:18

--- src/main/java/com/agoda/web/minwoo/FlightController.java
+++ src/main/java/com/agoda/web/minwoo/FlightController.java
@@ -1,10 +1,26 @@
 package com.agoda.web.minwoo;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FlightController {
 	private static final Logger logger = LoggerFactory.getLogger(FlightController.class);
+	
+	@RequestMapping(value="/flight/{id}/search", method=RequestMethod.POST, consumes="application/json")
+	public Map<?, ?> bookflight(@PathVariable("id") String id, @RequestBody HashMap<String,String> param) {
+		Map<String, String> map = new HashMap<>();
+		logger.info("컨트롤러 진입{}", "welcome");
+		logger.info("파람값{}", param);
+		
+		return null;
+	}
 }
